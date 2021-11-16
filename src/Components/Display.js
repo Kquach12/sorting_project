8f0a4faa6243a3817a9cbc96066006d5318be070
Kenny Quach <kennyquach60@gmail.com>
2021-11-15 21:42:33 -0800

    Add delay in loop to visibly show algo in progress

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,21 +4,23 @@ import React, {useState} from 'react'
 
 
 const Display = (props) => {
-    const [testArray,setTestArray] = useState([5,2,10,15,20,4,1,15,40,45,23,10,37,3,7,9,35,3,45])
+    const [testArray,setTestArray] = useState([5,2,1,15,20,4,1,15,40,45,23,10,37,3,7,9,35,3,45])
 
-    const insertionSort = (array)=> {
-        // console.log(array)
+    const timer = ms => new Promise(res => setTimeout(res, ms))
+
+    async function insertionSort(array) {
+        
         for (let i = 1; i < array.length; i++){
             let j = i
             while (j > 0 && array[j] < array[j-1]){
                 swap (j, j-1, array)
                 j-=1
+                setTestArray([...array])
+                await timer(300)
             }
+            setTestArray([...array])
+            await timer(300)
         }
-        console.log(testArray)
-
-        setTestArray(array)
-        console.log(testArray)
     }
 
     function swap(i, j, array){
